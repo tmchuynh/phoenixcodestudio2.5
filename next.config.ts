1912f59c98f5d92ce2cd3759bc9839ab50b16fc5
Tina Huynh <tinamchuynh@gmail.com>
2025-04-10 22:41:39 -0700

    chore: update dependencies and add Radix UI components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+const imageHosts = [
+  "tailwindcss.com",
+  "images.unsplash.com",
+  "plus.unsplash.com",
+  "media.istockphoto.com",
+  "www.shutterstock.com",
+  "cdn.sanity.io",
+];
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: imageHosts.map((hostname) => ({
+      protocol: "https",
+      hostname,
+      port: "",
+      pathname: "/**",
+    })),
+    dangerouslyAllowSVG: true,
+  },
 };
 
 export default nextConfig;
