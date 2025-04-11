a610ee4d563c0f02e9dda3d839b0b575f7a9a9f1
Tina Huynh <tinamchuynh@gmail.com>
2025-04-11 16:00:27 -0700

    Refactors Footer component and adds new image
    Renames the Example component to Footer for clarity.
    
    Improves styling and layout of the footer section, including updated image properties for better responsiveness.
    
    Fixes text hierarchy by changing headings from h3 to h4.
    
    Addresses accessibility with improved link styles.
    
    Adds a new image for branding purposes.

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -26,30 +26,30 @@ const navigation = {
   ],
 };
 
-export default function Example() {
+export default function Footer() {
   return (
-    <footer className="bg-white">
-      <div className="mx-auto px-6 lg:px-8 py-16 sm:py-24 lg:py-32 max-w-7xl">
+    <footer className="mt-6 md:mt-12 lg:mt-16 xl:mt-24 border-t">
+      <div className="mx-auto px-6 lg:px-8 py-6 md:py-12 lg:py-20 max-w-7xl">
         <div className="xl:gap-8 xl:grid xl:grid-cols-3">
           <Image
             alt="Company name"
-            src="/images/logo_white.png"
-            className="h-9"
-            width={36}
-            height={36}
+            src="/images/logo.png"
+            className="object-cover object-center aspect-auto"
+            width={300}
+            height={300}
           />
           <div className="gap-8 grid grid-cols-2 xl:col-span-2 mt-16 xl:mt-0">
             <div className="md:gap-8 md:grid md:grid-cols-2">
               <div>
-                <h3 className="font-semibold text-gray-900 text-sm/6">
+                <h4>
                   Solutions
-                </h3>
+                </h4>
                 <ul role="list" className="space-y-4 mt-6">
                   {navigation.solutions.map( ( item ) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-gray-600 text-sm/6 hover:text-gray-900"
+                        className="text-sm/6 underline-offset-4 hover:underline"
                       >
                         {item.name}
                       </a>
@@ -58,15 +58,15 @@ export default function Example() {
                 </ul>
               </div>
               <div className="mt-10 md:mt-0">
-                <h3 className="font-semibold text-gray-900 text-sm/6">
+                <h4>
                   Support
-                </h3>
+                </h4>
                 <ul role="list" className="space-y-4 mt-6">
                   {navigation.support.map( ( item ) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-gray-600 text-sm/6 hover:text-gray-900"
+                        className="text-sm/6 underline-offset-4 hover:underline"
                       >
                         {item.name}
                       </a>
@@ -77,15 +77,15 @@ export default function Example() {
             </div>
             <div className="md:gap-8 md:grid md:grid-cols-2">
               <div>
-                <h3 className="font-semibold text-gray-900 text-sm/6">
+                <h4>
                   Company
-                </h3>
+                </h4>
                 <ul role="list" className="space-y-4 mt-6">
                   {navigation.company.map( ( item ) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-gray-600 text-sm/6 hover:text-gray-900"
+                        className="text-sm/6 underline-offset-4 hover:underline"
                       >
                         {item.name}
                       </a>
@@ -94,13 +94,13 @@ export default function Example() {
                 </ul>
               </div>
               <div className="mt-10 md:mt-0">
-                <h3 className="font-semibold text-gray-900 text-sm/6">Legal</h3>
+                <h4>Legal</h4>
                 <ul role="list" className="space-y-4 mt-6">
                   {navigation.legal.map( ( item ) => (
                     <li key={item.name}>
                       <a
                         href={item.href}
-                        className="text-gray-600 text-sm/6 hover:text-gray-900"
+                        className="text-sm/6 underline-offset-4 hover:underline"
                       >
                         {item.name}
                       </a>
