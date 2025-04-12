12c3b1c76320aa429c7091b10c19bea4f5b2b7c4
Tina Huynh <tinamchuynh@gmail.com>
2025-04-11 19:01:52 -0700

    Refines About and Past Projects pages layout
    Simplifies the structure of the About PCS page by removing unnecessary styling and enhancing readability.
    
    Introduces SplitScreenshot component to the Past Projects page for improved visual presentation.
    
    Updates headings for consistency and clarity.

--- src/app/past_projects/page.tsx
+++ src/app/past_projects/page.tsx
@@ -1,11 +1,16 @@
+import SplitScreenshot from "@/components/headers/page_headers/SplitScreenshot";
+
 export default function PastProjects() {
   return (
-    <div className="pt-3 md:pt-5 lg:pt-9">
-      <h1 className="font-bold text-4xl text-center">Past Projects</h1>
-      <p className="mt-4 text-center">
-        Here are some of the projects we've worked on in the past.
-      </p>
-      {/* Add your past projects content here */}
-    </div>
+    <>
+      <SplitScreenshot />
+      <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
+        <h1>Past Projects</h1>
+        <p className="mt-4 text-center">
+          Here are some of the projects we've worked on in the past.
+        </p>
+        {/* Add your past projects content here */}
+      </div>
+    </>
   );
 }
