6c5771fe50cbd721334245ee1e75e2b1256d0fb0
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 01:58:12 -0700

    Increases grid gap for blog cards
    Adjusts the gap between blog cards for improved spacing
    Enhances layout aesthetics and readability
    
    No related issues

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,7 +7,7 @@ export default function BlogPage() {
       <div className="mx-auto px-6 lg:px-8">
         <h4>Deploy faster</h4>
         <h1>Hear What Our Community Has to Say About Tech's Latest Updates</h1>
-        <div className="gap-4 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 mt-10 sm:mt-16">
+        <div className="gap-6 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 mt-10 sm:mt-16">
           {blogs.map((blog, index) => (
             <BlogCard key={index} blog={blog} />
           ))}
