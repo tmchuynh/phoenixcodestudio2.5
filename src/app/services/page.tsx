61d84b887bf0ca8f19a4436d1a84f6b1ffc9a047
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 01:57:53 -0700

    Refactor ServicesPage component to enhance layout, integrate FeaturedLargeTestimonial, and improve navigation with dynamic buttons

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import DynamicButton from "@/components/button/button-dynamic";
+import FeaturedLargeTestimonial from "@/components/cards/testimonial/Large_Avatar";
 import IncentivesImage from "@/components/headers/page_headers/IncentivesImage";
+import ContactInfoSpliter from "@/components/page_spliter/ContactInfo";
 import LoadingIndicator from "@/components/states/loading/Loading";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { allServices } from "@/lib/constants/services/serviceCategories";
 import { subServiceDetails } from "@/lib/constants/services/subServices";
+import { testimonials } from "@/lib/constants/testimonials";
 import useSmallScreen from "@/lib/screens/useSmallScreen";
 import { capitalize, generateSlug } from "@/lib/utils";
 import { useTheme } from "next-themes";
@@ -43,10 +46,25 @@ const ServicesPage: FC = () => {
     <main className="mx-auto py-6 w-10/12 md:w-11/12">
       <IncentivesImage />
 
+      <FeaturedLargeTestimonial testimonial={testimonials[0]} />
+
       {/* Company Services Section */}
       {allServices.map((category, index) => (
         <div key={index} className="my-20">
           <h1>{capitalize(category.name)}</h1>
+          <div className="my-4">
+            {category.description.map((desc, descIndex) => (
+              <p key={descIndex} className="max-w-6xl">
+                {desc}
+              </p>
+            ))}
+            <DynamicButton
+              className="mx-0 w-1/3"
+              onClick={() => router.push("/get_in_touch")}
+            >
+              Contact Us Today!
+            </DynamicButton>
+          </div>
           <div className="gap-4 lg:gap-6 grid grid-cols-1 lg:grid-cols-2">
             {category.sub.map((subService, subIndex) => {
               const subServiceDetail = subServiceDetails.find(
@@ -60,7 +78,17 @@ const ServicesPage: FC = () => {
                     className="flex flex-col justify-between shadow-md"
                   >
                     <CardContent className="flex flex-col">
-                      <h3>{capitalize(subServiceDetail.name)}</h3>
+                      <h3
+                        className="font-semibold text-lg underline-offset-4 hover:underline cursor-pointer"
+                        onClick={() =>
+                          navigateToCategory(
+                            category.name,
+                            subServiceDetail.name
+                          )
+                        }
+                      >
+                        {capitalize(subServiceDetail.name)}
+                      </h3>
 
                       {isSmallScreen ? (
                         <p>{subServiceDetail.info.short}</p>
@@ -74,24 +102,6 @@ const ServicesPage: FC = () => {
                         </>
                       )}
                     </CardContent>
-
-                    <CardFooter>
-                      <DynamicButton
-                        onClick={() =>
-                          navigateToCategory(
-                            category.name,
-                            subServiceDetail.name
-                          )
-                        }
-                      >
-                        View More Details
-                      </DynamicButton>
-                      <DynamicButton
-                        onClick={() => router.push("/get_in_touch")}
-                      >
-                        Contact Us Today!
-                      </DynamicButton>
-                    </CardFooter>
                   </Card>
                 );
               }
@@ -99,6 +109,8 @@ const ServicesPage: FC = () => {
           </div>
         </div>
       ))}
+
+      <ContactInfoSpliter />
     </main>
   );
 };
