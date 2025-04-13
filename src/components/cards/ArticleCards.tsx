1cf686f25c612bbe00aed68331168c8ab246fcfb
Tina Huynh <tinamchuynh@gmail.com>
2025-04-12 19:27:23 -0700

    Refactors BlogCard component for improved clarity
    Removes unused Image import and comments out the Image component for future implementation.
    
    Simplifies class names in CardTitle and CardDescription for better readability.
    
    Updates the router path in the Read More button to correct URL structure.

--- src/components/cards/ArticleCards.tsx
+++ src/components/cards/ArticleCards.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -5,7 +7,6 @@ import {
   generateSlug,
   parseReadingTimeToMinutes,
 } from "@/lib/utils";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import {
   Card,
@@ -22,20 +23,18 @@ export default function BlogCard({ blog }: { blog: any }) {
   return (
     <Card className="shadow-lg hover:shadow-2xl border hover:border-gray-200 border-transparent rounded-2xl max-w-md transition">
       <CardHeader className="relative h-56">
-        <Image
+        {/* <Image
           src={`/images/blog_card_images/${generateSlug(blog.title)}.jpg`}
           alt={blog.title}
           layout="fill"
           className="rounded-t-2xl object-cover"
           priority
-        />
+        /> */}
       </CardHeader>
       <CardContent className="p-4">
-        <CardTitle className="mb-2 font-bold text-2xl text-gray-900">
-          {blog.title}
-        </CardTitle>
+        <CardTitle className="mb-2 font-bold text-2xl">{blog.title}</CardTitle>
         {(blog.wordCount || blog.time) && (
-          <div className="flex justify-between mb-2 text-gray-500 text-sm">
+          <div className="flex justify-between mb-2 text-sm">
             {blog.wordCount && (
               <span>
                 <strong>Words:</strong> {formatNumber(blog.wordCount)}
@@ -48,9 +47,7 @@ export default function BlogCard({ blog }: { blog: any }) {
             )}
           </div>
         )}
-        <CardDescription className="mb-4 text-gray-700">
-          {blog.excerpt}
-        </CardDescription>
+        <CardDescription className="mb-4">{blog.excerpt}</CardDescription>
         {blog.topics && blog.topics.length > 0 && (
           <div className="flex flex-wrap gap-2">
             {blog.topics.sort().map((topic: string, index: number) => (
@@ -63,7 +60,7 @@ export default function BlogCard({ blog }: { blog: any }) {
       </CardContent>
       <CardFooter className="px-4 pt-0 pb-4">
         <Button
-          onClick={() => router.push(`/info/blogs/${generateSlug(blog.title)}`)}
+          onClick={() => router.push(`/blogs/${generateSlug(blog.title)}`)}
         >
           Read More
         </Button>
