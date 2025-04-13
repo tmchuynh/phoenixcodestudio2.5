a5cadce9bcfa12947f1d375e14b0210745a3ff17
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 01:58:01 -0700

    Updates FAQs page content and structure
    Refines FAQs page to enhance user engagement with clearer messaging and a more inviting tone.
    
    Replaces the loading indicator with immediate content display, improving user experience.
    
    Switches to a dynamic button for better functionality and updates the accordion header styling for improved readability.

--- src/app/get_in_touch/frequently_asked_questions/page.tsx
+++ src/app/get_in_touch/frequently_asked_questions/page.tsx
@@ -1,52 +1,50 @@
 "use client";
 
+import DynamicButton from "@/components/button/button-dynamic";
 import CallToAction from "@/components/CTA/Simple";
-import LoadingIndicator from "@/components/states/loading/Loading";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { Button } from "@/components/ui/button";
 import { FAQs } from "@/lib/constants/frequently-asked-questions";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export default function FrequentlyAskedQuestions() {
   const router = useRouter();
-  const [loading, setLoading] = useState(true);
-
-  if (loading) {
-    return <LoadingIndicator />;
-  }
 
   return (
     <main className="mx-auto py-6 w-10/12 md:w-11/12">
-      <h1 className="font-bold text-2xl">Welcome to Our FAQs Page!</h1>
+      <h1>FAQs: Your Digital Journey Starts Here</h1>
       <p className="mt-4">
-        Got questions? We’ve got answers! This page is your go-to resource for
-        quick, clear information about our services, products, and processes.
-        Whether you're exploring our offerings, seeking guidance, or
-        troubleshooting an issue, you’ll find solutions right here.
+        Welcome to the Phoenix Code Studio FAQs page, where we address common
+        questions about our services, process, and how we can help your business
+        thrive online. Whether you’re new to digital transformation or looking
+        to deepen your understanding of our approach, you’ll find answers that
+        guide you towards informed decisions and successful outcomes.
+      </p>
+      <p>
+        Explore below for insights into our methodologies, capabilities, and how
+        we empower businesses like yours to lead in the digital age.
       </p>
       <p className="mt-4">
         Can’t find what you’re looking for?{" "}
-        <Button
+        <DynamicButton
           variant="link"
           onClick={() => router.push("/contact-us")}
           className="m-0 p-0 h-fit"
         >
           Reach out to us anytime
-        </Button>
-        - we’re happy to help!
+        </DynamicButton>
+        we’re happy to help!
       </p>
       <section className="mb-8">
         <Accordion type="single" collapsible className="w-full">
           {FAQs.map((question, index) => (
             <AccordionItem value={`${index}`} key={index}>
               <AccordionTrigger className="text-start">
-                {question.question}
+                <h3>{question.question}</h3>
               </AccordionTrigger>
               <AccordionContent>
                 <p>{question.answer}</p>
