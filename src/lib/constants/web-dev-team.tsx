c1cf703940586055b5fc5706b0a2a9627409a83a
Tina Huynh <tinamchuynh@gmail.com>
2025-04-12 21:50:38 -0700

    Adds web development team constants
    Introduces a new module for the web development team
    with structured information about team members and their
    responsibilities to enhance content management.

---
+++ src/lib/constants/web-dev-team.tsx
@@ -0,0 +1,12 @@
+export const webDevTeam = [
+  { name: "Your Team", content: <p>Content for Your Team.</p> },
+  {
+    name: "Our Design Team",
+    content: <p>Content for Our Design Team.</p>,
+  },
+  { name: "Our Developers", content: <p>Content for Our Developers.</p> },
+  {
+    name: "Our Content Creators",
+    content: <p>Content for Our Content Creators.</p>,
+  },
+];
