cf59c028e4008370cb98d5a7cb3d655fcf3291f6
Tina Huynh <tinamchuynh@gmail.com>
2025-04-12 19:27:27 -0700

    Enhances blog page with dynamic blog cards
    Implements rendering of blog cards using data from the blogs array.
    
    Updates the layout to accommodate three columns for better presentation.
    
    Improves user experience by showcasing community insights effectively.

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,16 @@
+import BlogCard from "@/components/cards/ArticleCards";
+import { blogs } from "@/lib/constants/blog-posts";
+
 export default function BlogPage() {
   return (
     <div className="mx-auto py-24 sm:py-32 w-11/12">
       <div className="mx-auto px-6 lg:px-8">
         <h4>Deploy faster</h4>
         <h1>Hear What Our Community Has to Say About Tech's Latest Updates</h1>
-        <div className="gap-4 grid grid-cols-1 lg:grid-cols-6 lg:grid-rows-2 mt-10 sm:mt-16">
-          {/* Content goes here */}
+        <div className="gap-4 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 mt-10 sm:mt-16">
+          {blogs.map((blog, index) => (
+            <BlogCard key={index} blog={blog} />
+          ))}
         </div>
       </div>
     </div>
