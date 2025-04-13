a60d40fae014b0167cf1d237a4d1cbfc7b29193a
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 01:57:46 -0700

    Refactor CallToAction component to update content and replace anchor tags with DynamicButton for improved functionality

--- src/components/CTA/Simple.tsx
+++ src/components/CTA/Simple.tsx
@@ -1,20 +1,26 @@
+import { useRouter } from "next/navigation";
+import DynamicButton from "../button/button-dynamic";
+
 export default function CallToAction() {
+  const router = useRouter();
   return (
     <div className="">
       <div className="mx-auto px-6 lg:px-8 py-24 sm:py-32 max-w-7xl">
-        <h2 className="max-w-2xl font-semibold text-4xl text-balance text-gray-900 sm:text-5xl tracking-tight">
-          Boost your productivity. Start using our app today.
-        </h2>
+        <h2>Ready to Bring Your Vision to Life?</h2>
+        <p>
+          Whether you're starting from scratch, refreshing your brand, or
+          scaling to the next level, Phoenix Code Studio is here to help you
+          rise above the noise. Our team is ready to collaborate, strategize,
+          and create a digital experience that reflects your unique identity and
+          drives real results.
+        </p>
+        <p>Let’s build something meaningful—together.</p>
         <div className="flex items-center gap-x-6 mt-10">
-          <a
-            href="#"
-            className="bg-indigo-600 hover:bg-indigo-500 shadow-xs px-3.5 py-2.5 rounded-md font-semibold text-sm text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          <DynamicButton
+            onClick={() => router.push("/get_in_touch/lets_work_together")}
           >
             Get started
-          </a>
-          <a href="#" className="font-semibold text-gray-900 text-sm/6">
-            Learn more <span aria-hidden="true">→</span>
-          </a>
+          </DynamicButton>
         </div>
       </div>
     </div>
