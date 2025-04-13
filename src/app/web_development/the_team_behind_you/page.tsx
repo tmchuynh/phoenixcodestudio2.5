1b4493d9b6c572aee37c0fcb71fa192e0fd64c17
Tina Huynh <tinamchuynh@gmail.com>
2025-04-12 21:50:51 -0700

    Refactors layout of TheTeamBehindYou component for improved structure and readability

--- src/app/web_development/the_team_behind_you/page.tsx
+++ src/app/web_development/the_team_behind_you/page.tsx
@@ -1,17 +1,16 @@
+import TabSections from "./TabSections";
+
 export default function TheTeamBehindYou() {
   return (
     <main className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-      <div className="mx-auto px-4 lg:max-w-none">
-        <div className="items-center gap-x-16 gap-y-10 grid grid-cols-1 lg:grid-cols-2">
-          <div>
-            <h1>Meet Your Talented Team</h1>
-            <p className="mt-4 text-gray-500">
-              We are a team of passionate developers, designers, and marketers
-              who are dedicated to helping you succeed online.
-            </p>
-          </div>
-        </div>
+      <div>
+        <h1>Meet Your Talented Team</h1>
+        <p className="mt-4">
+          We are a team of passionate developers, designers, and marketers who
+          are dedicated to helping you succeed online.
+        </p>
       </div>
+      <TabSections />
     </main>
   );
 }
