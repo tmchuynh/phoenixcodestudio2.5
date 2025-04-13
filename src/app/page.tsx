46eb83e945cf8fae7d866486309c4830bf9d9900
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 01:56:45 -0700

    Adds featured articles section to homepage
    Implements a new section to display featured articles on the homepage.
    
    Utilizes React hooks to manage state and effects for fetching articles.
    
    Integrates existing components to enhance the user interface.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import HeaderImageTiles from "@/components/headers/page_headers/HeaderImageTiles";
+"use client";
 
-export default function Example() {
+import FeaturedArticles from "@/components/cards/FeaturedArticles";
+import Simple from "@/components/CTA/Simple";
+import HeaderImageTiles from "@/components/headers/page_headers/HeaderImageTiles";
+import { blogs } from "@/lib/constants/blog-posts";
+import { BlogPost } from "@/lib/interfaces/blogs";
+import { featuredArray } from "@/lib/utils";
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+  const [featuredArticles, setFeaturedArticles] = useState<BlogPost[]>([]);
+
+  useEffect(() => {
+    setFeaturedArticles(featuredArray(blogs));
+  }, []);
   return (
     <div className="pt-3 md:pt-5 lg:pt-9">
       <HeaderImageTiles />
+
+      <div className="space-y-10 mx-auto w-11/12">
+        {featuredArticles.slice(0, 4).map((article, index) => (
+          <FeaturedArticles key={index} blog={article} />
+        ))}
+      </div>
+
+      <Simple />
     </div>
   );
 }
