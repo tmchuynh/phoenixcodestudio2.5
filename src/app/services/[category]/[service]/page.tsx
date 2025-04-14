b5630be58c10cfe4f5e9d7062e3a5edcfd691166
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 23:33:10 -0700

    Refactors service page component structure
    Improves rendering logic to conditionally display service data.
    Simplifies introduction and pricing sections for better clarity.
    Enhances layout by integrating details and CTA components.
    
    Relates to service improvements.

--- src/app/services/[category]/[service]/page.tsx
+++ src/app/services/[category]/[service]/page.tsx
@@ -7,7 +7,6 @@ import CannotFind from "@/components/states/not-found/CannotFind";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { SubItem } from "@/lib/interfaces/services";
 import { capitalize } from "@/lib/utils";
-import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -65,63 +64,80 @@ export default function ServicePage() {
 
   return (
     <>
-      <main className="mx-auto py-6 w-10/12 md:w-11/12">
-        <h1>{serviceName}</h1>
-        <p className="mb-4">{serviceData?.info.short}</p>
-
-        {serviceData?.info.pricing.pricingTierIntro?.title && (
-          <div className="mt-4">
-            <h2>{serviceData.info.pricing.pricingTierIntro.title}</h2>
-            <p>{serviceData.info.pricing.pricingTierIntro.description}</p>
-          </div>
-        )}
-
-        <h2>Do You Have Questions?</h2>
-        <p>
-          Whether you’re just starting your digital transformation journey or
-          seeking to learn more about our process, our FAQs are here to help.
-          Discover clear answers to your questions and gain the insights you
-          need to make confident, informed decisions.
-        </p>
-        <p>
-          Explore our FAQs to learn about our methodologies, capabilities, and
-          how we help businesses like yours thrive in the digital era.
-        </p>
-        <DynamicButton
-          variant={"secondaryOutline"}
-          className="mx-0 w-1/3"
-          onClick={() =>
-            router.push("/get_in_touch/frequently_asked_questions")
-          }
-        >
-          Visit The FAQs
-        </DynamicButton>
-
-        {serviceData?.info.pricing.pricingTiers && (
-          <div className="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-7">
-            {serviceData.info.pricing.pricingTiers.map((prices, index) => (
-              <Card key={index}>
-                <CardHeader>
-                  <Image
-                    src={"https://placehold.co/600x400"}
-                    alt={prices.name}
-                    width={800}
-                    height={600}
-                    className="rounded object-cover object-center"
-                  />
-                  <CardTitle>
-                    <h3 className="py-3">{prices.name}</h3>
-                  </CardTitle>
-                </CardHeader>
-                <CardContent>
-                  <p>{prices.info}</p>
-                </CardContent>
-              </Card>
+      {serviceData && (
+        <>
+          <main className="mx-auto py-6 w-10/12 md:w-11/12">
+            <h1>{serviceName}</h1>
+            {serviceData.info.intro.map((intro, index) => (
+              <p key={index}>{intro}</p>
             ))}
-          </div>
-        )}
-      </main>
-      {serviceData?.cta && <CallToActionImage cta={serviceData.cta} />}
+
+            {serviceData.info.pricing.pricingTierIntro?.title && (
+              <div className="mt-4">
+                <h2>{serviceData.info.pricing.pricingTierIntro.title}</h2>
+                <p>{serviceData.info.pricing.pricingTierIntro.description}</p>
+              </div>
+            )}
+
+            <h3>Do You Have Questions?</h3>
+            <p>
+              Whether you’re just starting your digital transformation journey
+              or seeking to learn more about our process, our FAQs are here to
+              help. Discover clear answers to your questions and gain the
+              insights you need to make confident, informed decisions.
+            </p>
+            <p>
+              Explore our FAQs to learn about our methodologies, capabilities,
+              and how we help businesses like yours thrive in the digital era.
+            </p>
+            <DynamicButton
+              variant={"secondaryOutline"}
+              className="mx-0 w-1/3"
+              onClick={() =>
+                router.push("/get_in_touch/frequently_asked_questions")
+              }
+            >
+              Visit The FAQs
+            </DynamicButton>
+
+            {serviceData.info.pricing.pricingTiers && (
+              <div className="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-7">
+                {serviceData.info.pricing.pricingTiers.map((prices, index) => (
+                  <Card key={index}>
+                    <CardHeader>
+                      <CardTitle>
+                        <h3>{prices.name}</h3>
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <p>{prices.info}</p>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+            )}
+
+            {serviceData.details.map((info, index) => (
+              <section key={index}>
+                <h2>{info.title}</h2>
+                {info.intro?.map((sentence, sIndex) => (
+                  <p key={sIndex}>{sentence}</p>
+                ))}
+
+                <div className="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-4">
+                  {info.lists?.map((listInfo, listIndex) => (
+                    <div key={listIndex}>
+                      <h4>{listInfo.title}</h4>
+                      <p>{listInfo.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </section>
+            ))}
+          </main>
+          {serviceData.cta && <CallToActionImage cta={serviceData.cta} />}
+        </>
+      )}
     </>
   );
 }
