3d42fb2124fc6461ced44032a9d248b600e483e9
Tina Huynh <tinamchuynh@gmail.com>
2025-04-13 23:33:20 -0700

    Improves padding for project display section
    Enhances the layout by adjusting bottom padding
    to ensure better spacing in the project display area.
    
    No functional changes were made.

--- src/app/past_projects/page.tsx
+++ src/app/past_projects/page.tsx
@@ -6,8 +6,7 @@ export default function PastProjects() {
   return (
     <>
       <SplitScreenshot />
-      <div className="mx-auto pt-3 md:pt-5 lg:pt-9 w-11/12">
-        <h1>Past Projects</h1>
+      <div className="mx-auto pt-3 md:pt-5 lg:pt-9 pb-10 md:pb-12 lg:pb-18 2xl:pb-24 w-11/12">
         <div className="gap-5 grid grid-cols-1 lg:grid-cols-2 py-2 md:py-4 2xl:py-6">
           {pastProjects.map((project, index) => (
             <ProjectCards key={index} project={project} />
