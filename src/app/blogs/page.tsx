50c11f4347bcce6b58fab3699e423109df2b301e
Tina Huynh <tinamchuynh@gmail.com>
2025-04-16 21:24:26 -0700

    Improves layout spacing in blog section
    Increases gap spacing for blog cards to enhance visual clarity and responsiveness.
    
    Adjusts grid layout for better alignment on different screen sizes.

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -7,7 +7,7 @@ export default function BlogPage() {
       <div className="mx-auto px-6 lg:px-8">
         <h4>The pulse of tech, through your peers</h4>
         <h1>Hear What Our Community Has to Say About Tech's Latest Updates</h1>
-        <div className="gap-6 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 mt-10 sm:mt-16">
+        <div className="gap-9 md:gap-12 lg:gap-16 grid grid-cols-1 lg:grid-cols-3 lg:grid-rows-2 mt-10 sm:mt-16">
           {blogs.map((blog, index) => (
             <BlogCard key={index} blog={blog} />
           ))}
