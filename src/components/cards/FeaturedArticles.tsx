f7e27b7c3df9d51a4077e8a3794e466ac55858c8
Tina Huynh <tinamchuynh@gmail.com>
2025-04-18 17:12:32 -0700

    Refactors FeaturedArticles and FeaturedProjects for better UX
    Wraps article and project sections in links for improved navigation.
    Enhances styling for titles on hover and optimizes component structure.
    
    Relates to user feedback for better accessibility.

--- src/components/cards/FeaturedArticles.tsx
+++ src/components/cards/FeaturedArticles.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { BlogPost } from "@/lib/interfaces/blogs";
 import useSmallScreen from "@/lib/screens/useSmallScreen";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-import DynamicButton from "../button/button-dynamic";
 import { cn } from "@/lib/utils";
 import { convertToDate } from "@/lib/utils/convert";
 import { generateSlug } from "@/lib/utils/format";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import DynamicButton from "../button/button-dynamic";
 
 export default function FeaturedArticles({
   blog,
@@ -19,37 +20,41 @@ export default function FeaturedArticles({
   const isSmallScreen = useSmallScreen();
 
   return (
-    <section
-      className={cn(
-        "flex flex-col-reverse md:flex-col items-center gap-3 md:gap-4 lg:gap-10 lg:grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 2xl:grid-cols-7 mx-2 md:mx-0",
-        {
-          "border-t py-10": isSmallScreen && index !== 0,
-          "pb-10": isSmallScreen && index === 0,
-        }
-      )}
-    >
-      <Image
-        src={blog.imageUrl || "https://placehold.co/600x400"}
-        alt={blog.title}
-        className="md:col-span-1 lg:col-span-3 rounded-2xl w-full h-full object-cover"
-        width={600}
-        height={400}
-      />
+    <Link href={`/blogs/${generateSlug(blog.title)}`} className="group">
+      <section
+        className={cn(
+          "flex flex-col-reverse md:flex-col items-center gap-3 md:gap-4 lg:gap-10 lg:grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 2xl:grid-cols-7 mx-2 md:mx-0",
+          {
+            "border-t py-10": isSmallScreen && index !== 0,
+            "pb-10": isSmallScreen && index === 0,
+          }
+        )}
+      >
+        <Image
+          src={blog.imageUrl}
+          alt={blog.title}
+          className="md:col-span-1 lg:col-span-3 rounded-2xl w-full h-full object-cover"
+          width={600}
+          height={400}
+        />
 
-      <div className="flex flex-col justify-between md:col-span-2 lg:col-span-3 2xl:col-span-4 py-2 h-full">
-        <h4>{convertToDate(blog.date)}</h4>
-        <h2>{blog.title}</h2>
+        <div className="flex flex-col justify-between md:col-span-2 lg:col-span-3 2xl:col-span-4 py-2 h-full">
+          <h4>{convertToDate(blog.date)}</h4>
+          <h2 className="underline-offset-4 group-hover:underline decoration-2">
+            {blog.title}
+          </h2>
 
-        <p className="max-w-5xl line-clamp-6">{blog.intro}</p>
+          <p className="max-w-5xl line-clamp-6">{blog.intro}</p>
 
-        <DynamicButton
-          variant={"tertiaryOutline"}
-          className="mx-0 w-2/3"
-          onClick={() => router.push(`/blogs/${generateSlug(blog.title)}`)}
-        >
-          Read More
-        </DynamicButton>
-      </div>
-    </section>
+          <DynamicButton
+            variant={"tertiaryOutline"}
+            className="mx-0 w-2/3"
+            onClick={() => router.push(`/blogs/${generateSlug(blog.title)}`)}
+          >
+            Read More
+          </DynamicButton>
+        </div>
+      </section>
+    </Link>
   );
 }
--- src/components/cards/FeaturedProjects.tsx
+++ src/components/cards/FeaturedProjects.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { Project } from "@/lib/interfaces/projects";
 import useSmallScreen from "@/lib/screens/useSmallScreen";
 import { cn } from "@/lib/utils";
-import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FaCheckDouble } from "react-icons/fa";
 import { TbBrandGithubFilled } from "react-icons/tb";
 import DynamicButton from "../button/button-dynamic";
 import { Badge } from "../ui/badge";
-import { Project } from "@/lib/interfaces/projects";
 
 export function FeaturedProject({
   project,
@@ -21,95 +21,94 @@ export function FeaturedProject({
   const router = useRouter();
 
   return (
-    <div
-      className={cn("flex flex-col lg:grid grid-cols-5", {
-        "border-t py-10": isSmallScreen && index !== 0,
-        "pb-10": isSmallScreen && index === 0,
-      })}
-    >
-      <div className="lg:col-span-5 xl:col-span-3 p-8 md:p-10">
-        <h2>{project.title}</h2>
-        <div className="flex flex-wrap items-center gap-4 mt-6">
-          {project.tags.map((tag, index) => (
-            <Badge key={index}>{tag}</Badge>
-          ))}
+    <Link href={project.githubLink || project.liveLink || ""} className="group">
+      <section
+        className={cn("flex flex-col lg:grid grid-cols-5", {
+          "border-t py-10": isSmallScreen && index !== 0,
+          "pb-10": isSmallScreen && index === 0,
+        })}
+      >
+        <div className="lg:col-span-5 xl:col-span-3 p-8 md:p-10">
+          <h2 className="underline-offset-4 group-hover:underline decoration-2">
+            {project.title}
+          </h2>
+          <div className="flex flex-wrap items-center gap-4 mt-6">
+            {project.tags.map((tag, index) => (
+              <Badge key={index}>{tag}</Badge>
+            ))}
+          </div>
+          <p className="mt-6 text-base/7">{project.description}</p>
+          <div className="flex md:flex-row xl:flex-row flex-col lg:flex-col gap-4 lg:gap-2 xl:gap-3 pt-4">
+            {project.githubLink && (
+              <DynamicButton
+                variant={"tertiaryOutline"}
+                onClick={() => router.push(project.githubLink || "")}
+                icon={TbBrandGithubFilled}
+                className="w-full md:w-1/2"
+              >
+                GitHub
+              </DynamicButton>
+            )}
+            {project.liveLink && (
+              <DynamicButton
+                variant={"secondaryOutline"}
+                onClick={() => router.push(project.liveLink || "")}
+                className="w-full md:w-1/2"
+              >
+                View Project Live
+              </DynamicButton>
+            )}
+          </div>
+          <ul
+            role="list"
+            className="gap-4 sm:gap-6 grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 mt-5 text-sm/6"
+          >
+            {project.tech.languages.map((language, index) => (
+              <li key={index} className="flex gap-x-3">
+                <FaCheckDouble
+                  aria-hidden="true"
+                  className="flex-none w-5 h-6"
+                />
+                {language}
+              </li>
+            ))}
+
+            {project.tech.frameworks &&
+              project.tech.frameworks.map((framework, index) => (
+                <li key={index} className="flex gap-x-3">
+                  <FaCheckDouble
+                    aria-hidden="true"
+                    className="flex-none w-5 h-6"
+                  />
+                  {framework}
+                </li>
+              ))}
+
+            {project.tech.libraries &&
+              project.tech.libraries.map((library, index) => (
+                <li key={index} className="flex gap-x-3">
+                  <FaCheckDouble
+                    aria-hidden="true"
+                    className="flex-none w-5 h-6"
+                  />
+                  {library}
+                </li>
+              ))}
+
+            {project.tech.technologies &&
+              project.tech.technologies.map((technology, index) => (
+                <li key={index} className="flex gap-x-3">
+                  <FaCheckDouble
+                    aria-hidden="true"
+                    className="flex-none w-5 h-6"
+                  />
+                  {technology}
+                </li>
+              ))}
+          </ul>
         </div>
-        <p className="mt-6 text-base/7">{project.description}</p>
-        <div className="flex md:flex-row xl:flex-row flex-col lg:flex-col gap-4 lg:gap-2 xl:gap-3 pt-4">
-          {project.githubLink && (
-            <DynamicButton
-              variant={"tertiaryOutline"}
-              onClick={() => router.push(project.githubLink || "")}
-              icon={TbBrandGithubFilled}
-              className="w-full md:w-1/2"
-            >
-              GitHub
-            </DynamicButton>
-          )}
-          {project.liveLink && (
-            <DynamicButton
-              variant={"secondaryOutline"}
-              onClick={() => router.push(project.liveLink || "")}
-              className="w-full md:w-1/2"
-            >
-              View Project Live
-            </DynamicButton>
-          )}
-        </div>
-        <ul
-          role="list"
-          className="gap-4 sm:gap-6 grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 mt-5 text-sm/6"
-        >
-          {project.tech.languages.map((language, index) => (
-            <li key={index} className="flex gap-x-3">
-              <FaCheckDouble aria-hidden="true" className="flex-none w-5 h-6" />
-              {language}
-            </li>
-          ))}
-
-          {project.tech.frameworks &&
-            project.tech.frameworks.map((framework, index) => (
-              <li key={index} className="flex gap-x-3">
-                <FaCheckDouble
-                  aria-hidden="true"
-                  className="flex-none w-5 h-6"
-                />
-                {framework}
-              </li>
-            ))}
-
-          {project.tech.libraries &&
-            project.tech.libraries.map((library, index) => (
-              <li key={index} className="flex gap-x-3">
-                <FaCheckDouble
-                  aria-hidden="true"
-                  className="flex-none w-5 h-6"
-                />
-                {library}
-              </li>
-            ))}
-
-          {project.tech.technologies &&
-            project.tech.technologies.map((technology, index) => (
-              <li key={index} className="flex gap-x-3">
-                <FaCheckDouble
-                  aria-hidden="true"
-                  className="flex-none w-5 h-6"
-                />
-                {technology}
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className="xl:block lg:hidden xl:col-span-2 mx-4 md:mx-0 -mt-2 lg:mt-0 p-2 lg:w-full lg:max-w-lg xl:max-w-none lg:shrink-0">
-        <Image
-          src={project.imageUrl || "https://placehold.co/600x400"}
-          alt={project.title}
-          className="rounded-2xl w-full h-full object-contain xl:object-cover object-center"
-          width={800}
-          height={800}
-        />
-      </div>
-    </div>
+        <div className="xl:block lg:hidden xl:col-span-2 mx-4 md:mx-0 -mt-2 lg:mt-0 p-2 border-4 rounded-4xl lg:w-full lg:max-w-lg xl:max-w-none lg:shrink-0"></div>
+      </section>
+    </Link>
   );
 }
