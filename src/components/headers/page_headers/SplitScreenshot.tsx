238482d5eb9746298148a7f45b2b97be6c61b7eb
Tina Huynh <tinamchuynh@gmail.com>
2025-04-18 18:33:09 -0700

    Updates image source in SplitScreenshot component for improved visual relevance

--- src/components/headers/page_headers/SplitScreenshot.tsx
+++ src/components/headers/page_headers/SplitScreenshot.tsx
@@ -45,7 +45,7 @@ export default function SplitScreenshot() {
             <div className="bg-gray-900/5 -m-2 lg:-m-4 p-2 lg:p-4 rounded-xl lg:rounded-2xl ring-1 ring-gray-900/10 ring-inset">
               <Image
                 alt=""
-                src="https://img.freepik.com/free-photo/programming-background-with-person-working-with-codes-computer_23-2150010123.jpg?ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+                src="https://img.freepik.com/free-photo/closeup-computer-laptop-screen-showing-ideas-wooden-table_53876-16088.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
                 width={2432}
                 height={1442}
                 className="shadow-2xl rounded-md ring-1 ring-gray-900/10 w-[76rem]"
