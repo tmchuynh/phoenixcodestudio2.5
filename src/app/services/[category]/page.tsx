676b835b097d3f38294dbaa751816eebe9e08d92
Tina Huynh <tinamchuynh@gmail.com>
2025-04-18 17:53:55 -0700

    Enhances category page with improved service links
    Replaces static button with navigable card links for sub-services.
    
    Introduces card components to display service details, including pricing tiers and additional information, improving user navigation and interaction.
    
    Updates UI elements for better responsiveness and aesthetics.

--- src/app/services/[category]/page.tsx
+++ src/app/services/[category]/page.tsx
@@ -2,12 +2,18 @@
 import DynamicButton from "@/components/button/button-dynamic";
 import LoadingIndicator from "@/components/states/loading/Loading";
 import CannotFind from "@/components/states/not-found/CannotFind";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { subServiceDetails } from "@/lib/constants/services/subServices";
 import { Category } from "@/lib/interfaces/services";
 import useSmallScreen from "@/lib/screens/useSmallScreen";
-import { generateSlug, capitalize } from "@/lib/utils/format";
+import { capitalize, generateSlug } from "@/lib/utils/format";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -74,14 +80,6 @@ export default function CategoryPage() {
       {service && (
         <main className="mx-auto py-6 w-10/12 md:w-11/12">
           <h1>{service?.name && capitalize(service?.name)}</h1>
-          <DynamicButton
-            variant={theme === "dark" ? "outline" : "accent"}
-            className="mx-0"
-            onClick={() => navigateToDetails(service.name)}
-          >
-            View More Details
-          </DynamicButton>
-
           <p>{service?.description}</p>
 
           <div className="gap-4 lg:gap-8 grid grid-cols-1 md:grid-cols-2 py-6 lg:py-10">
@@ -92,18 +90,39 @@ export default function CategoryPage() {
 
               if (subServiceDetail) {
                 return (
-                  <Card className="shadow-md px-5 py-10" key={subIndex}>
-                    <h3>{capitalize(subServiceDetail.name)} – Pricing</h3>
-                    <ul className="space-y-4 mx-8 list-disc list-outside">
-                      {subServiceDetail.info.pricing.pricingTiers.map(
-                        (tier, tierIndex) => (
-                          <li key={tierIndex}>
-                            <strong>{tier.name}:</strong> {tier.info}
-                          </li>
-                        )
-                      )}
-                    </ul>
-                  </Card>
+                  <Link
+                    href={`/services/${category}/${generateSlug(
+                      subServiceDetail.name
+                    )}`}
+                    key={subIndex}
+                    className="group"
+                  >
+                    <Card className="flex flex-col justify-between shadow-md px-5 py-10 h-full">
+                      <CardHeader>
+                        <h3 className="underline-offset-4 group-hover:underline decoration-2">
+                          {capitalize(subServiceDetail.name)}
+                        </h3>
+                        <p>{subServiceDetail.info.short}</p>
+                      </CardHeader>
+                      <CardContent className="h-full">
+                        <ul className="space-y-4 mx-8 list-disc list-outside">
+                          {subServiceDetail.info.pricing.pricingTiers.map(
+                            (tier, tierIndex) => (
+                              <li key={tierIndex}>
+                                <strong>{tier.name}:</strong> {tier.info}
+                              </li>
+                            )
+                          )}
+                        </ul>
+                      </CardContent>
+                      <CardFooter className="flex flex-col gap-6">
+                        <DynamicButton className="mx-0 place-self-start">
+                          More Information
+                        </DynamicButton>
+                        <h5>{subServiceDetail.info.hook}</h5>
+                      </CardFooter>
+                    </Card>
+                  </Link>
                 );
               }
 
