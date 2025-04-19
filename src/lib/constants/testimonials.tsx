13b451e2fa1fa29c9405e065d96fd2bd71fdf897
Tina Huynh <tinamchuynh@gmail.com>
2025-04-18 17:12:10 -0700

    Enhances testimonial formatting for improved visibility
    Updates testimonial quotes to include styled markup for key phrases, enhancing readability and visual appeal.
    
    Improves consistency in presentation across all testimonials.

--- src/lib/constants/testimonials.tsx
+++ src/lib/constants/testimonials.tsx
@@ -9,16 +9,23 @@ export const testimonials: Testimonial[] = [
     quote: (
       <p>
         Tina was responsible for developing and designing our new website, and
-        her <mark>creativity has taken us to the next level</mark>. From start
-        to finish, she completely transformed the way our schools perceive our
-        programs. As someone who likes to be hands-on throughout the process, it
-        was a pleasant surprise to entrust Tina with the project and have{" "}
-        <mark>all of our expectations exceeded</mark>. She now has full control
-        to make updates and changes as needed. Her unique blend of creativity
-        and business acumen is exceptional. We've gone from a simple, self-built
-        website to one that truly represents the professionalism and experience
-        of a company with over 20 years in the industry. Tina has played a key
-        role in presenting our company in the best possible way.
+        her{" "}
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          creativity has taken us to the next level
+        </mark>
+        . From start to finish, she completely transformed the way our schools
+        perceive our programs. As someone who likes to be hands-on throughout
+        the process, it was a pleasant surprise to entrust Tina with the project
+        and have{" "}
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          all of our expectations exceeded
+        </mark>
+        . She now has full control to make updates and changes as needed. Her
+        unique blend of creativity and business acumen is exceptional. We've
+        gone from a simple, self-built website to one that truly represents the
+        professionalism and experience of a company with over 20 years in the
+        industry. Tina has played a key role in presenting our company in the
+        best possible way.
       </p>
     ),
   },
@@ -30,8 +37,11 @@ export const testimonials: Testimonial[] = [
     quote: (
       <p>
         Tina has an exceptional talent for web design and brings ideas to life
-        in a way that is both <mark>visually striking and functional</mark>. She
-        has a keen eye for detail and truly understands the importance of
+        in a way that is both{" "}
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          visually striking and functional
+        </mark>
+        . She has a keen eye for detail and truly understands the importance of
         translating vision into an engaging digital experience. Her approach is
         very intentional, Tina’s creativity shines through in every project she
         works on, and she consistently delivers results that exceed
@@ -49,14 +59,16 @@ export const testimonials: Testimonial[] = [
         Tina is a dynamic full-stack web developer with a passion for crafting
         intuitive and functional digital experiences. Specializing in user
         experience and front-end design, she brings{" "}
-        <mark>creativity and precision</mark> to every project, showcasing her
-        strong work ethic and unwavering dedication. Her ability to blend
-        technical expertise with a keen eye for design makes her a standout in
-        her field. Beyond development, Tina is a true polymath, seamlessly
-        integrating skills from software engineering, business development, real
-        estate, and personal growth. Her ability to connect these diverse
-        disciplines fuels her innovation, allowing her to create unique,
-        forward-thinking solutions that drive real impact.
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          creativity and precision
+        </mark>{" "}
+        to every project, showcasing her strong work ethic and unwavering
+        dedication. Her ability to blend technical expertise with a keen eye for
+        design makes her a standout in her field. Beyond development, Tina is a
+        true polymath, seamlessly integrating skills from software engineering,
+        business development, real estate, and personal growth. Her ability to
+        connect these diverse disciplines fuels her innovation, allowing her to
+        create unique, forward-thinking solutions that drive real impact.
       </p>
     ),
   },
@@ -67,17 +79,22 @@ export const testimonials: Testimonial[] = [
     position: "Data Scientist",
     quote: (
       <p>
-        It's not often you meet someone as <mark>driven and talented</mark> as
-        Tina. I had the pleasure of working alongside her at ******** at
+        It's not often you meet someone as{" "}
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          driven and talented
+        </mark>{" "}
+        as Tina. I had the pleasure of working alongside her at ******** at
         California State University, Fullerton. Her leadership stood out
         immediately—she consistently took initiative, introduced innovative
         ideas, and guided the team through challenges with confidence and
         resilience. Beyond her leadership, Tina's eagerness to learn and adapt
         is truly inspiring. Whether mastering technical skills or building a
         business, she embraces new challenges with{" "}
-        <mark>enthusiasm and determination</mark>. Her dedication,
-        resourcefulness, and unwavering drive make her an invaluable asset to
-        any team.
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          enthusiasm and determination
+        </mark>
+        . Her dedication, resourcefulness, and unwavering drive make her an
+        invaluable asset to any team.
       </p>
     ),
   },
@@ -93,14 +110,16 @@ export const testimonials: Testimonial[] = [
         she has grown into a highly skilled and proficient developer. Her
         ability to learn and adapt is evident in her work, particularly
         showcased on her personal portfolio site—
-        <mark>
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
           an impressive testament to her technical expertise and creativity
         </mark>
         . What sets Tina apart is her{" "}
-        <mark>relentless drive and self-motivation</mark>. Her projects clearly
-        demonstrate her ability to tackle new technologies with confidence,
-        overcoming any challenge that comes her way. She is a true
-        problem-solver and an invaluable asset to any team.
+        <mark className="bg-primary/20 shadow-sm px-1 py-0.5 rounded-md">
+          relentless drive and self-motivation
+        </mark>
+        . Her projects clearly demonstrate her ability to tackle new
+        technologies with confidence, overcoming any challenge that comes her
+        way. She is a true problem-solver and an invaluable asset to any team.
       </p>
     ),
   },
