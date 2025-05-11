826a0bda4dd1a8bff8aa422d288f6bf9e0db8054
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 16:57:25 -0700

    Update FeaturedImageGallery component with new default image and adjust styling for better responsiveness

--- src/components/images/FeaturedImage.tsx
+++ src/components/images/FeaturedImage.tsx
@@ -6,18 +6,18 @@ import React from "react";
 
 export function FeaturedImageGallery() {
   const [active, setActive] = React.useState(
-    "https://img.freepik.com/premium-vector/motivational-quote-black-white-poster-design_525160-7502.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740"
+    "https://plus.unsplash.com/premium_photo-1681505195930-388c317b7a76?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3VwcG9ydHxlbnwwfDB8MHx8fDA%3D"
   );
 
   return (
     <div className="gap-4 grid">
       <div>
         <Image
-          className="rounded-lg w-full max-w-full h-auto md:h-[480px] object-cover object-center"
+          className="rounded-lg w-full h-auto md:h-[20em] aspect-video object-cover object-center"
           src={active}
           alt=""
           width={1470}
-          height={980}
+          height={1200}
         />
       </div>
       <div className="gap-4 grid grid-cols-5">
