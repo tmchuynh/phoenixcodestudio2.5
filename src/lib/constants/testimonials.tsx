c5ab5336a7999e8bc955e05bc90da25da60ec9b0
Tina Huynh <tinamchuynh@gmail.com>
2025-05-11 17:08:24 -0700

    Refactor testimonials component to improve quote formatting and update image URLs for enhanced presentation

--- src/lib/constants/testimonials.tsx
+++ src/lib/constants/testimonials.tsx
@@ -9,25 +9,18 @@ export const testimonials: Testimonial[] = [
     quote: (
       <p>
         Tina was responsible for developing and designing our new website, and
-        her{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          creativity has taken us to the next level
-        </mark>
-        . From start to finish, she completely transformed the way our schools
-        perceive our programs. As someone who likes to be hands-on throughout
-        the process, it was a pleasant surprise to entrust Tina with the project
-        and have{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          all of our expectations exceeded
-        </mark>
-        . She now has full control to make updates and changes as needed. Her
-        unique blend of creativity and business acumen is exceptional. We've
-        gone from a simple, self-built website to one that truly represents the
-        professionalism and experience of a company with over 20 years in the
-        industry. Tina has played a key role in presenting our company in the
-        best possible way. I highly recommend her to anyone looking to elevate
-        their online presence. I look forward to working with her on future
-        projects.
+        her creativity has taken us to the next level. From start to finish, she
+        completely transformed the way our schools perceive our programs. As
+        someone who likes to be hands-on throughout the process, it was a
+        pleasant surprise to entrust Tina with the project and have all of our
+        expectations exceeded. She now has full control to make updates and
+        changes as needed. Her unique blend of creativity and business acumen is
+        exceptional. We've gone from a simple, self-built website to one that
+        truly represents the professionalism and experience of a company with
+        over 20 years in the industry. Tina has played a key role in presenting
+        our company in the best possible way. I highly recommend her to anyone
+        looking to elevate their online presence. I look forward to working with
+        her on future projects.
       </p>
     ),
   },
@@ -39,20 +32,17 @@ export const testimonials: Testimonial[] = [
     quote: (
       <p>
         Tina has an exceptional talent for web design and brings ideas to life
-        in a way that is both{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          visually striking and functional
-        </mark>
-        . She has a keen eye for detail and truly understands the importance of
-        translating vision into an engaging digital experience. Her approach is
-        very intentional, Tina’s creativity shines through in every project she
-        works on, and she consistently delivers results that exceed
-        expectations. Her ability to blend aesthetics with functionality is a
-        rare skill that sets her apart in the industry. I highly recommend Tina
-        to anyone looking to elevate their online presence. She is a true
-        professional who is dedicated to her craft and always goes the extra
-        mile to ensure her clients are satisfied. I look forward to working with
-        her on future projects.
+        in a way that is both visually striking and functional. She has a keen
+        eye for detail and truly understands the importance of translating
+        vision into an engaging digital experience. Her approach is very
+        intentional, Tina’s creativity shines through in every project she works
+        on, and she consistently delivers results that exceed expectations. Her
+        ability to blend aesthetics with functionality is a rare skill that sets
+        her apart in the industry. I highly recommend Tina to anyone looking to
+        elevate their online presence. She is a true professional who is
+        dedicated to her craft and always goes the extra mile to ensure her
+        clients are satisfied. I look forward to working with her on future
+        projects.
       </p>
     ),
   },
@@ -65,11 +55,8 @@ export const testimonials: Testimonial[] = [
       <p>
         Tina is a dynamic full-stack web developer with a passion for crafting
         intuitive and functional digital experiences. Specializing in user
-        experience and front-end design, she brings{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          creativity and precision
-        </mark>{" "}
-        to every project, showcasing her strong work ethic and unwavering
+        experience and front-end design, she brings creativity and precision to
+        every project, showcasing her strong work ethic and unwavering
         dedication. Her ability to blend technical expertise with a keen eye for
         design makes her a standout in her field. Beyond development, Tina is a
         true polymath, seamlessly integrating skills from software engineering,
@@ -96,26 +83,20 @@ export const testimonials: Testimonial[] = [
     position: "Data Scientist",
     quote: (
       <p>
-        It's not often you meet someone as{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          driven and talented
-        </mark>{" "}
-        as Tina. I had the pleasure of working alongside her at ******** at
-        California State University, Fullerton. Her leadership stood out
-        immediately—she consistently took initiative, introduced innovative
-        ideas, and guided the team through challenges with confidence and
-        resilience. Beyond her leadership, Tina's eagerness to learn and adapt
-        is truly inspiring. Whether mastering technical skills or building a
-        business, she embraces new challenges with{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          enthusiasm and determination
-        </mark>
-        . Her dedication, resourcefulness, and unwavering drive make her an
-        invaluable asset to any team. I have no doubt that she will continue to
-        excel in her career and make a significant impact in the tech industry.
-        I wholeheartedly recommend Tina to anyone seeking a talented and driven
-        professional. Her unique blend of skills, creativity, and dedication
-        will undoubtedly elevate any project she undertakes.
+        It's not often you meet someone as driven and talented as Tina. I had
+        the pleasure of working alongside her at ******** at California State
+        University, Fullerton. Her leadership stood out immediately—she
+        consistently took initiative, introduced innovative ideas, and guided
+        the team through challenges with confidence and resilience. Beyond her
+        leadership, Tina's eagerness to learn and adapt is truly inspiring.
+        Whether mastering technical skills or building a business, she embraces
+        new challenges with enthusiasm and determination . Her dedication,
+        resourcefulness, and unwavering drive make her an invaluable asset to
+        any team. I have no doubt that she will continue to excel in her career
+        and make a significant impact in the tech industry. I wholeheartedly
+        recommend Tina to anyone seeking a talented and driven professional. Her
+        unique blend of skills, creativity, and dedication will undoubtedly
+        elevate any project she undertakes.
       </p>
     ),
   },
@@ -130,22 +111,16 @@ export const testimonials: Testimonial[] = [
         the pleasure of working with. Despite starting with little experience,
         she has grown into a highly skilled and proficient developer. Her
         ability to learn and adapt is evident in her work, particularly
-        showcased on her personal portfolio site—
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          an impressive testament to her technical expertise and creativity
-        </mark>
-        . What sets Tina apart is her{" "}
-        <mark className="bg-primary/20 dark:bg-secondary shadow-sm px-1 py-0.5 rounded-md">
-          relentless drive and self-motivation
-        </mark>
-        . Her projects clearly demonstrate her ability to tackle new
-        technologies with confidence, overcoming any challenge that comes her
-        way. She is a true problem-solver and an invaluable asset to any team. I
-        have no doubt that she will continue to excel in her career and make a
-        significant impact in the tech industry. I wholeheartedly recommend Tina
-        to anyone seeking a talented and driven professional. Her unique blend
-        of skills, creativity, and dedication will undoubtedly elevate any
-        project she undertakes.
+        showcased on her personal portfolio site— an impressive testament to her
+        technical expertise and creativity . What sets Tina apart is her{" "}
+        relentless drive and self-motivation . Her projects clearly demonstrate
+        her ability to tackle new technologies with confidence, overcoming any
+        challenge that comes her way. She is a true problem-solver and an
+        invaluable asset to any team. I have no doubt that she will continue to
+        excel in her career and make a significant impact in the tech industry.
+        I wholeheartedly recommend Tina to anyone seeking a talented and driven
+        professional. Her unique blend of skills, creativity, and dedication
+        will undoubtedly elevate any project she undertakes.
       </p>
     ),
   },
@@ -175,22 +150,74 @@ export const testimonials: Testimonial[] = [
 export const testimonialImages = [
   {
     imageUrl:
-      "https://img.freepik.com/premium-vector/motivational-quote-black-white-poster-design_525160-7502.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740",
+      "https://images.unsplash.com/photo-1455734729978-db1ae4f687fc?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
   },
   {
     imageUrl:
-      "https://img.freepik.com/free-photo/quote-paper-note-with-encouragement-words_23-2150105151.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740",
+      "https://images.unsplash.com/photo-1516149893016-813d9a01d5d3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
   },
   {
     imageUrl:
-      "https://img.freepik.com/free-photo/creative-inspirational-resource_23-2149144221.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740",
+      "https://images.unsplash.com/photo-1474314881477-04c4aac40a0e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTYyfHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
   },
   {
     imageUrl:
-      "https://img.freepik.com/free-photo/beautiful-inspirational-quote-message_23-2149261758.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740",
+      "https://images.unsplash.com/photo-1511632765486-a01980e01a18?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c3VwcG9ydHxlbnwwfDB8MHx8fDA%3D",
   },
   {
     imageUrl:
-      "https://img.freepik.com/free-photo/quote-paper-note-with-encouragement-words_23-2150105153.jpg?uid=R93718308&ga=GA1.1.1699338801.1739848115&semt=ais_hybrid&w=740",
+      "https://images.unsplash.com/photo-1454909516657-78526f214d05?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc0fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1527058054345-f870ffe763eb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjY2fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1490578474895-699cd4e2cf59?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1536607774613-b287a2f4e698?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzQwfHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1519671482749-fd09be7ccebf?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1520880867055-1e30d1cb001c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1446160657592-4782fb76fb99?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTExfHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1491438590914-bc09fcaaf77a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzJ8fGZyaWVuZHN8ZW58MHwwfDB8fHww",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1574740637579-9ca0a610e491?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM1fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1505258867551-9a6e9dddb3ef?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ3fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1473642676276-2d4ab561542e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM5fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
+  },
+  {
+    imageUrl:
+      "https://plus.unsplash.com/premium_photo-1661719880750-4c0de579cd09?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZnJpZW5kc3xlbnwwfDB8MHx8fDA%3D",
+  },
+  {
+    imageUrl:
+      "https://images.unsplash.com/photo-1496275068113-fff8c90750d1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTY3fHxmcmllbmRzfGVufDB8MHwwfHx8MA%3D%3D",
   },
 ];
